61ca5886b25c17ddf0cebb7280ad15b1eb6ca1d7
Haythem <sinnner6@gmail.com>
2023-01-26 22:05:27 +0100

    implemented footer component

--- src/components/container/Footer/Footer.jsx
+++ src/components/container/Footer/Footer.jsx
@@ -3,8 +3,13 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div className="container" id="footer">
-      Footer
+    <div className="footer">
+      <div className="copyright">
+        <p>
+          {/* Copyright&copy;2022 All rights reserved. |  */}
+          Made with 🧡 and ☕.
+        </p>
+      </div>
     </div>
   );
 };
