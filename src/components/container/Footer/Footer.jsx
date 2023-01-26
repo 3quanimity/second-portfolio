8beec573feeba503fde32dd74026e0a543a41fc1
Haythem <sinnner6@gmail.com>
2023-01-26 23:16:59 +0100

    animated footer

--- src/components/container/Footer/Footer.jsx
+++ src/components/container/Footer/Footer.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Footer.scss";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <motion.div
+      className="footer"
+      initial={{ opacity: 0 }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{ duration: 1.5 }}
+    >
       <div className="copyright">
         <p>
           {/* Copyright&copy;2022 All rights reserved. |  */}
           Made with 🧡 and ☕.
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
